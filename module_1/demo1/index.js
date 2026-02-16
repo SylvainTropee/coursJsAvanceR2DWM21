391194140323deb88d00159e53c51283bf0cfed6
stropee <stropee@campus-eni.fr>
2026-02-16 11:30:35 +0100

    Module 1 demo 1

--- module_1/demo1/index.js
+++ module_1/demo1/index.js
@@ -15,9 +15,60 @@ console.log(typeof size);
 
 console.groupEnd()
 
-
-
 ////////////////////////////////
+
+//structures de codes
+
+for(let i= 0; i < clothes.length; i++){
+  console.log(clothes[i]);
+}
+console.log("-------------");
+for (const clothe of clothes) {
+  console.log(clothe);
+}
+console.log("-------------");
+clothes.forEach(
+  function(value, index){
+    console.log(`Index : ${index} -> ${value}`);
+    // console.log(this);
+  }
+)
+console.log("-------------");
+clothes.forEach(
+  (value, index) => {
+    console.log(`Index : ${index} -> ${value}`);
+    // console.log(this);
+  }
+)
+
+
+//functions
+function getClothe(index){
+  return clothes[index];
+}
+//anonyme
+const getClothe2 = function(index){
+  return clothes[index];
+}
+console.log(getClothe2(3));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 

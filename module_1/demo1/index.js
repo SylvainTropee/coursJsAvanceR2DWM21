f320aa0e04ca691d37ae2489d65bf38bcd85ba23
stropee <stropee@campus-eni.fr>
2026-02-16 10:32:03 +0100

    Module 1 demo 1

---
+++ module_1/demo1/index.js
@@ -0,0 +1,19 @@
+//déclarations
+
+const trouser = 'jean';
+let size = 42;
+
+let clothes = ['dress', 'short', 't-shirt', 'bob'];
+
+console.group("Habits")
+console.log(trouser);
+console.log(size);
+console.table(clothes);
+
+console.log(typeof trouser);
+console.log(typeof size);
+
+console.groupEnd()
+
+
+

c55ddfe6a986d051506cefe2ad87bcad9f5a8f11
stropee <stropee@campus-eni.fr>
2026-02-17 12:01:21 +0100

    Module 2 demos

---
+++ module_2/demo3/script.js
@@ -0,0 +1,22 @@
+function Character(name, species = 'Gundan', age = null) {
+  this.name = name;
+  this.species = species;
+  this.age = age;
+  this.description = function() {
+    return `${this.name}, ${this.species}`;
+  };
+}
+
+const chewie = new Character('Chewbacca', 'Wookie', 200)
+const jarjar = new Character('Jar Jar')
+
+chewie.weapon = "Sabre laser"
+Character.prototype.weapon = "Arbalète laser";
+
+// console.log(chewie.name);
+// chewie.age = 150;
+// console.log(chewie);
+// console.log(chewie.description());
+// chewie.weapon = 'Arbalète laser';
+console.log(chewie.weapon);
+console.log(jarjar.weapon);

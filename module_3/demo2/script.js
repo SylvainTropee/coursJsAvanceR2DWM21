d8ffe98bb61eb1004dfc651ecb266bf60812dae5
stropee <stropee@campus-eni.fr>
2026-02-18 10:28:02 +0100

    Module 3 API

---
+++ module_3/demo2/script.js
@@ -0,0 +1,23 @@
+let characters = [];
+
+ fetch("https://hp-api.onrender.com/api/characters")
+   .then((httpResponse) => httpResponse.json())
+   .then((data) => {
+     console.log(data);
+
+     characters = data;
+     console.log(characters);
+     for(const character of data){
+
+       const p = document.createElement('p')
+       p.innerText = character.name
+
+       const img = document.createElement('img')
+       img.src = character.image
+
+       document.body.append(p)
+       document.body.append(img)
+     }
+ })
+
+console.log(characters);
